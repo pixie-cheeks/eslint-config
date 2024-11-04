85886aeaf488666301877bea8dea503c1a6a4ecd
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-11-04 22:39:12 +0530

    Fix incorrect export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import base from './configs/base.js';
 import onlyReact from './configs/only-react.js';
-import react from './airbnb-react/react.js';
+import react from './configs/react.js';
 
 const configs = {
   base,
