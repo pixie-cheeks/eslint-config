7c741114248468e00f63d3277f12dad62debe4c6
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-11-04 23:20:46 +0530

    Stop using filename-case unciorn rule
    What files it should be applied on seems more project-dependent.

--- configs/react.js
+++ configs/react.js
@@ -1,12 +1,4 @@
 import base from './base.js';
 import onlyReact from './only-react.js';
 
-export default [
-  ...base,
-  ...onlyReact,
-  {
-    rules: {
-      'unicorn/filename-case': ['error', { cases: { pascalCase: true } }],
-    },
-  },
-];
+export default [...base, ...onlyReact];
