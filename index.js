bcc7052025c416f1e2ba36ef9bb93ed1bf1bf41e
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-11-04 15:42:14 +0530

    Replace airbnb node rules with plugin-n
    They were deprecated and weren't used anyway.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import bestPractices from './airbnb/best-practices.js';
 import errors from './airbnb/errors.js';
 import es6 from './airbnb/es6.js';
 import imports from './airbnb/imports.js';
-import node from './airbnb/node.js';
 import strict from './airbnb/strict.js';
 import style from './airbnb/style.js';
 import variables from './airbnb/variables.js';
@@ -10,6 +9,7 @@ import globals from 'globals';
 import configPretter from 'eslint-config-prettier';
 import stylistic from '@stylistic/eslint-plugin';
 import pluginUnicorn from 'eslint-plugin-unicorn';
+import pluginNode from 'eslint-plugin-n';
 
 const styleRules = {
   '@stylistic/lines-between-class-members': [
@@ -29,11 +29,11 @@ export default [
   errors,
   es6,
   imports,
-  node,
   strict,
   style,
   variables,
   pluginUnicorn.configs['flat/recommended'],
+  pluginNode.configs['flat/recommended-module'],
   {
     languageOptions: {
       globals: {
