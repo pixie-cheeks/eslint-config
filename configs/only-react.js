a673f91aeb891419a04dfdabe041d1f3f6c33a8e
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-11-04 23:07:42 +0530

    Make the additional rule mixed only
    The unicorn plugin will need fiddling to be separated from base
    config, so it will be mixed with the full react config for now.

--- configs/only-react.js
+++ configs/only-react.js
@@ -4,7 +4,6 @@ import reactHooks from '../airbnb-react/react-hooks.js';
 import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
-import pluginUnicorn from 'eslint-plugin-unicorn';
 import { defineFlatConfig } from 'eslint-define-config';
 import configPretter from 'eslint-config-prettier';
 import globals from 'globals';
@@ -18,7 +17,6 @@ export default defineFlatConfig([
       'jsx-a11y': pluginJsxA11y,
       'react-hooks': pluginReactHooks,
       react: pluginReact,
-      unicorn: pluginUnicorn,
     },
 
     languageOptions: {
@@ -30,9 +28,6 @@ export default defineFlatConfig([
           jsx: true,
         },
       },
-    },
-    rules: {
-      'unicorn/filename-case': ['error', { cases: { pascalCase: true } }],
     },
   },
   react,
--- configs/react.js
+++ configs/react.js
@@ -1,4 +1,12 @@
 import base from './base.js';
 import onlyReact from './only-react.js';
 
-export default [...base, ...onlyReact];
+export default [
+  ...base,
+  ...onlyReact,
+  {
+    rules: {
+      'unicorn/filename-case': ['error', { cases: { pascalCase: true } }],
+    },
+  },
+];
