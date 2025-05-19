6ba4753ce09f6b030addde0f59d9584fe4e64d19
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-19 13:16:37 +0530

    Add typedef for globals

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
 import { Linter } from 'eslint';
+import globals from 'globals';
+
+type Globals = typeof globals;
 
 type Configs = {
   base: Linter.Config[];
   react: Linter.Config[];
   prettier: Linter.Config;
+  globals: Globals;
 };
 
 declare const configs: Configs;
